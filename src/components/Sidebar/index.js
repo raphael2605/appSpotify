32f75c084d13f712b689217a874857ebc4f4135c
Raphael <raphael@fuerzastudio.com.br>
2020-01-08 17:09:53 -0200

    project init

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import { Container, NewPlaylist, Nav } from './styles';
+
+import AddPlaylistIcon from '../../assets/images/add_playlist.svg'
+
+const Sidebar = () => (
+  <Container>
+    <div>
+      <Nav main>
+        <li>
+          <a href="#">Navegar</a>
+        </li>
+        <li>
+          <a href="#">Rádio</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>SUA BIBLIOTECA</span>
+        </li>
+        <li>
+          <a href="#">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="#">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="#">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="#">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="#">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="#">Seu Daily Mix</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>PLAYLISTS</span>
+        </li>
+        <li>
+          <a href="#">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="#">Seu Daily Mix</a>
+        </li>
+      </Nav>
+    </div>
+    <NewPlaylist>
+      <img src={AddPlaylistIcon} alt="Adicionar playlist" />
+      Nova playlist
+    </NewPlaylist>
+  </Container>
+)
+
+export default Sidebar;

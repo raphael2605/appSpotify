304ae4c730c2e1c66bcd9535031175f4b3fbab90
Raphael <raphael@fuerzastudio.com.br>
2020-01-20 11:24:54 -0200

    Reverting to the state of the project at f414f312bd09394e929af979061ce785cb39a06cd3529ae

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -1,8 +1,4 @@
-import React, { Component } from 'react';
-
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Creators as PlaylistDetailsActions } from '../../store/ducks/playlistDetails';
+import React from 'react';
 
 import { Container, Header, SongList } from './styles';
 
@@ -10,61 +6,88 @@ import ClockIcon from '../../assets/images/clock.svg'
 
 import PlusIcon from '../../assets/images/plus.svg'
 
-class Playlist extends Component {
-  componentDidMount() {
-    this.loadPlaylistDetails();
-  }
+const Playlist = () => (
+  <Container>
+    <Header>
+      <img src="https://images.theconversation.com/files/258026/original/file-20190208-174861-nms2kt.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip" alt="album cover" />
 
-  loadPlaylistDetails = () => {
-    const { id } = this.props.match.params;
+      <div>
+        <span>PLAYLIST</span>
+        <h1>Rock Forever</h1>
+        <p>13 músicas</p>
+        <button>Play</button>
+      </div>
+    </Header>
 
-    this.props.getPlaylistDetailsRequest(id);
-  }
+    <SongList cellPadding={0} cellSpacing={0}>
+      <thead>
+        <th />
+        <th>Título</th>
+        <th>Artista</th>
+        <th>Álbum</th>
+        <th><img src={ClockIcon} alt="Duração" /></th>
+      </thead>
 
-  render() {
-    return (
-      <Container>
-        <Header>
-          <img src="https://images.theconversation.com/files/258026/original/file-20190208-174861-nms2kt.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip" alt="album cover" />
+      <tbody>
+        <tr>
+          <td><img src={PlusIcon} alt="Adicionar" /></td>
+          <td>Paper Cut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:28</td>
+        </tr>
+        <tr>
+          <td><img src={PlusIcon} alt="Adicionar" /></td>
+          <td>Paper Cut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:28</td>
+        </tr>
+        <tr>
+          <td><img src={PlusIcon} alt="Adicionar" /></td>
+          <td>Paper Cut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:28</td>
+        </tr>
+        <tr>
+          <td><img src={PlusIcon} alt="Adicionar" /></td>
+          <td>Paper Cut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:28</td>
+        </tr>
+        <tr>
+          <td><img src={PlusIcon} alt="Adicionar" /></td>
+          <td>Paper Cut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:28</td>
+        </tr>
+        <tr>
+          <td><img src={PlusIcon} alt="Adicionar" /></td>
+          <td>Paper Cut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:28</td>
+        </tr>
+        <tr>
+          <td><img src={PlusIcon} alt="Adicionar" /></td>
+          <td>Paper Cut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:28</td>
+        </tr>
+        <tr>
+          <td><img src={PlusIcon} alt="Adicionar" /></td>
+          <td>Paper Cut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:28</td>
+        </tr>
+      </tbody>
+    </SongList>
+  </Container>
+)
 
-          <div>
-            <span>PLAYLIST</span>
-            <h1>Rock Forever</h1>
-            <p>13 músicas</p>
-            <button>Play</button>
-          </div>
-        </Header>
-
-        <SongList cellPadding={0} cellSpacing={0}>
-          <thead>
-            <th />
-            <th>Título</th>
-            <th>Artista</th>
-            <th>Álbum</th>
-            <th><img src={ClockIcon} alt="Duração" /></th>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td><img src={PlusIcon} alt="Adicionar" /></td>
-              <td>Paper Cut</td>
-              <td>Linkin Park</td>
-              <td>Hybrid Theory</td>
-              <td>3:28</td>
-            </tr>
-          </tbody>
-        </SongList>
-      </Container>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  playlistDetails: state.playlistDetails,
-});
-
-const mapDispatchToProps = dispatch => bindActionCreators(PlaylistDetailsActions, dispatch);
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(Playlist);
-
+export default Playlist;
